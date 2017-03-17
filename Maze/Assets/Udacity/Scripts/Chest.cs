6bc80c70898fb962761bcda266afeae27687c61c
devreis <victorreis777@gmail.com>
2017-03-17 14:11:06 -0300

    style: prefabs update
    Updated with the new Udacity prefabs for this project

---
+++ Maze/Assets/Udacity/Scripts/Chest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour {
+
+	public GameObject top; 
+
+	public void OnChestClicked(){
+
+		top.transform.Rotate (50f, 0f, 0f);
+		
+	}
+}
--- Maze/Assets/Udacity/Scripts/Door.cs
+++ Maze/Assets/Udacity/Scripts/Door.cs
@@ -6,17 +6,20 @@ public class Door : MonoBehaviour
 {
     // Create a boolean value called "locked" that can be checked in Update() 
 
-	bool unlocked;
+    public bool unlocked;
     public AudioSource doorSound;
 	public AudioClip openGate;
 	public AudioClip closedGate;
-
+	public GameObject leftDoor;
+	public GameObject rightDoor;
 
 
 	public void OnDoorClicked(){
 		//
 		if (Key.gotKey == true) {
-			transform.Translate (0, 12f, 0, Space.World);
+			leftDoor.transform.Rotate (0f, 80f, 0f);
+			rightDoor.transform.Rotate (0f, 80f, 0f);
+			//transform.Translate (0, 15f, 0, Space.World);
 			doorSound.PlayOneShot (openGate);
 		} else {
 			doorSound.PlayOneShot (closedGate);
